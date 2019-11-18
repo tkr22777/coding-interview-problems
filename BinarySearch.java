6d676c282d513e59e6395e986d725d47f9010ffa
Tahsin Kabir <tahsinkabir@gmail.com>
2019-11-18 13:28:23 -0800

    Added BinarySearch

---
+++ BinarySearch.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class BinarySearch {
+
+    public static void main(String[] args) {
+        int[] sortedAr = {1, 2, 5, 7, 9, 11};
+        System.out.println("Given Array: " + Arrays.toString(sortedAr));
+        System.out.println(String.format("Search %s Result: %s", 5, search(sortedAr, 5)));
+        System.out.println(String.format("Search %s Result: %s", 9, search(sortedAr, 9)));
+        System.out.println(String.format("Search %s Result: %s", 6, search(sortedAr, 6)));
+    }
+
+    public static int search(int[] nums, int target) {
+
+        int start = 0;
+        int end  = nums.length - 1;
+
+        //Target should be in range num0 <= target <= numN
+        if (!(nums[0] <= target && target <= nums[end])) {
+            return -1;
+        }
+
+        while (start <= end) {
+                
+            int mid = (start + end) / 2;
+            
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (target > nums[mid]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
+
