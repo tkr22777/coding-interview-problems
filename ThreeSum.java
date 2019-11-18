d3cd6008d35109286802a2e5b73f624ec70b622f
Tahsin Kabir <tahsinkabir@gmail.com>
2019-11-18 13:16:36 -0800

    Added TS

---
+++ ThreeSum.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+public class ThreeSum {
+
+    public ThreeSum() {
+    }
+
+    public static void main(String[] args) {
+
+        int[] array = { -1, 0, 1, 2 , -1, -4};
+        List<List<Integer>> indexsWithZeroSum = threeSum(array);
+        for (List<Integer> indexes: indexsWithZeroSum) {
+            indexes.stream().forEach(System.out::print);
+            System.out.println();
+        }
+    }
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+
+        Map<Integer, List<List<Integer>>> twoSumToPairs = getTwoSumToOrderedPair(nums);
+
+        List<List<Integer>> solution = new ArrayList<>();
+
+        for (int k = 0 ; k < nums.length; k++) {
+
+            int key = -1 * nums[k];
+
+            if (twoSumToPairs.containsKey(key)) {
+                for (List<Integer> pair: twoSumToPairs.get(key)) {
+                    if (k > pair.get(0) && k > pair.get(1)) {
+                        List<Integer> triplet = new ArrayList(Arrays.asList(pair.get(0), pair.get(1), k));
+                        solution.add(triplet);
+                    }
+                }
+            }
+        }
+        return solution;
+    }
+
+    public static Map<Integer, List<List<Integer>>> getTwoSumToOrderedPair(int[] nums) {
+
+        Map<Integer, List<List<Integer>>> twoSumToPairs = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length ; j++) {
+                twoSumToPairs.computeIfAbsent(nums[i] + nums[j], t -> new ArrayList<>());
+                twoSumToPairs.get(nums[i] + nums[j]).add(Arrays.asList(i , j));
+            }
+        }
+
+        return twoSumToPairs;
+    }
+}
+
