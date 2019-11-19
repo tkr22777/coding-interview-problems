bac7e98dcec8507a341bf1c486ee3b46f7813b4d
Tahsin Kabir <tahsinkabir@gmail.com>
2019-11-18 22:27:48 -0800

    Binary string addition

---
+++ AddBinaryString.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class AddBinaryString {
+
+    public static void main(String[] args) {
+        String a = "1011001";
+        String b = "1011101";
+        System.out.println("  A: " + a);
+        System.out.println("  B: " + b);
+        System.out.println("A+B:" + addBinary(a, b));
+    }
+
+    public static String addBinary(String a, String b) {
+
+        int max_len = Math.max(a.length(), b.length());
+
+        boolean carry = false;
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < max_len ; i++) {
+
+            short currSum = 0;
+
+            if (a.length() - 1 - i >= 0 && a.charAt(a.length() - 1 - i) == '1') {
+                currSum++;
+            }
+
+            if (b.length() - 1 - i >= 0 && b.charAt(b.length() - 1 - i) == '1') {
+                currSum++;
+            }
+
+            if (carry) {
+                currSum++;
+            }
+
+            if (currSum == 0 || currSum == 2) {
+                sb.append("0");
+            } else {
+                sb.append("1");
+            }
+
+            carry = currSum > 1 ? true:false;
+        }
+
+        if (carry) {
+            sb.append("1");
+        }
+
+        return sb.reverse().toString();
+    }
+}
