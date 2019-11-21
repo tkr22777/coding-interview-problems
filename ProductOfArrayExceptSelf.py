177ed686e8e151b9419e3924b1156c8789e56111
Tahsin Kabir <tahsinkabir@gmail.com>
2019-11-21 15:40:57 -0800

    Added product of array except self

---
+++ ProductOfArrayExceptSelf.py
@@ -0,0 +1,37 @@
+class Solution(object):
+
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        map = {}
+
+        outputLeft = []
+        outputRight = []
+
+        for i in range(0, len(nums)):
+            outputLeft.append(1)
+            outputRight.append(1)
+
+        for i in range(0, len(nums)):
+            prev_index = i - 1;
+            if prev_index >= 0:
+                outputLeft[i] = outputLeft[prev_index] * nums[prev_index]
+
+            j = len(nums) - 1 - i;
+            next_index = j + 1;
+
+            if next_index < len(nums):
+                outputRight[j] = outputRight[next_index] * nums[next_index]
+
+        output = []
+        for i in range(0, len(nums)):
+            output.append(outputLeft[i] * outputRight[i])
+
+        return output
+
+s = Solution()
+print s.productExceptSelf([1, 2, 3])
+
+
