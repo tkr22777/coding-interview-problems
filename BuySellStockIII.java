3866675a5d33ba26673dd135e640a84fe743a81c
Tahsin Kabir <tahsinkabir@gmail.com>
2019-11-21 01:17:30 -0800

    Best time to buy and sell

---
+++ BuySellStockIII.java
@@ -0,0 +1,37 @@
+class BuySellStockIII {
+
+    public static void main(String[] args) {
+        int[] ar = {3,3,5,0,0,3,1,4};
+        System.out.println(new BuySellStockIII().maxProfit(ar));
+    }
+
+    public int maxProfit(int[] prices) {
+
+        int[] singleTxnProfit = new int[prices.length];
+
+        int maxProfit = 0;
+        int maxSellPrice = prices.length > 0 ? prices[prices.length - 1]: 0;
+        for (int i = prices.length - 1; i >= 0 ; --i) {
+            if (prices[i] > maxSellPrice) {
+                maxSellPrice = prices[i];
+            } else {
+                maxProfit = Math.max(maxProfit, maxSellPrice - prices[i]);
+            }
+            singleTxnProfit[i] = maxProfit;
+        }
+
+        maxProfit = 0;
+        int minBuyPrice = prices.length > 0 ? prices[0]: 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (minBuyPrice > prices[i]) {
+                minBuyPrice = prices[i];
+            } else {
+                int currentProfit = prices[i] - minBuyPrice;
+                int nextProfit = i + 1 < singleTxnProfit.length ? singleTxnProfit[i + 1]: 0;
+                maxProfit = Math.max(maxProfit, currentProfit + nextProfit);
+            }
+        }
+        return maxProfit;
+    }
+}
+
