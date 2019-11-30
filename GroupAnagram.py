6c56d4139ac6bdb26d5a0cd73cea547088fbfbfd
Tahsin Kabir <tahsinkabir@gmail.com>
2019-11-30 12:23:53 -0800

    Updated GroupAnagram.py

--- GroupAnagram.py
+++ GroupAnagram.py
@@ -1,28 +1,21 @@
 class Solution(object):
+    
     def groupAnagrams(self, strs):
         """
         :type strs: List[str]
         :rtype: List[List[str]]
         """
 
-        output = []
         outMap = {}
-
         for i in range(0, len(strs)):
 
             key = ''.join(sorted(strs[i]))
 
-            if key in outMap:
-                aList = outMap.get(key)
-                aList.append(strs[i])
-            else:
-                aList = []
-                aList.append(strs[i])
-                outMap[key] = aList
+            outMap.setdefault(key, []).append(strs[i])
 
+        output = []
         for val in outMap.values():
             output.append(val)
-
         return output
 
 
