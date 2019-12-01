6ebe44cf6b6159ae1f5afc07ec70e73c8b6462fd
Tahsin Kabir <tahsinkabir@gmail.com>
2019-11-30 17:58:40 -0800

    Added conversion to collection from Array examples

---
+++ JAVA/ConvertFromArray.java
@@ -0,0 +1,70 @@
+import java.util.*;
+import java.util.stream.*;
+
+/* 
+ * Takeaways, use Arrays.stream(arr) to create a stream.
+ * If the type of elements in the array is a primitive, use boxed()
+ */
+
+public class ConvertFromArray {
+
+    public static void main(String[] args) {
+
+        new ConvertFromArray().testArrays();
+    }
+
+    public void testArrays() {
+
+        int[] vals = {5, 1, 2, 3, 5, 3};
+        StringJoiner sjAr = new StringJoiner(", ", "Initial Array: ", ".");
+        Arrays.stream(vals).forEach(v -> sjAr.add(Integer.toString(v)));
+        System.out.println(sjAr.toString());
+
+        StringJoiner sjList = new StringJoiner(", ", "List collected from stream boxed array: ", ".");
+        List<Integer> integerList = Arrays.stream(vals).boxed().collect(Collectors.toList());
+        integerList.forEach(v -> sjList.add(Integer.toString(v)));
+        System.out.println(sjList.toString());
+            
+        StringJoiner sjArList = new StringJoiner(", ", "ArrayList created from array stream: ", ".");
+        ArrayList<Integer> integerArList = new ArrayList<Integer>();
+        Arrays.stream(vals).forEach(v -> integerArList.add(v));
+        integerArList.forEach(v -> sjArList.add(Integer.toString(v)));
+        System.out.println(sjArList.toString());
+
+        StringJoiner sjSet = new StringJoiner(", ", "Set collected from stream boxed array: ", ".");
+        Set<Integer> integerSet = Arrays.stream(vals).boxed().collect(Collectors.toSet());
+        integerSet.forEach(v -> sjSet.add(Integer.toString(v)));
+        System.out.println(sjSet.toString());
+            
+        StringJoiner sjHSet = new StringJoiner(", ", "LinkedHashSet collected from stream boxed array: ", ".");
+        LinkedHashSet<Integer> integerLHSet = new LinkedHashSet();
+        Arrays.stream(vals).forEach( v -> integerLHSet.add(v));
+        integerLHSet.forEach(v -> sjHSet.add(Integer.toString(v)));
+        System.out.println(sjHSet.toString());
+
+        StringJoiner sjMap = new StringJoiner(", ", "Map collected from stream boxed array: ", ".");
+        Map<Integer, Integer> intSquareMap = Arrays.stream(vals).boxed().distinct()
+            .collect(Collectors.toMap(
+                a -> a,
+                a -> a * a
+            ));
+        intSquareMap.entrySet().stream()
+            .forEach(es -> sjMap.add("{Key: " + es.getKey() + " Value: " + es.getValue() + "}"));
+        System.out.println(sjMap.toString());
+
+        StringJoiner sjLHMap = new StringJoiner(", ", "LinkedHashMap collected from stream boxed array: ", ".");
+        LinkedHashMap<Integer, Integer> intSquareLHMap = new LinkedHashMap<Integer, Integer>();
+        Arrays.stream(vals).boxed().forEach(v -> intSquareLHMap.put(v, v * v));
+        intSquareLHMap.entrySet().stream()
+            .forEach(es -> sjLHMap.add("{Key: " + es.getKey() + " Value: " + es.getValue() + "}"));
+        System.out.println(sjLHMap.toString());
+
+        StringJoiner sjHistMap = new StringJoiner(", ", "Histogram created from stream boxed array: ", ".");
+        HashMap<Integer, Integer> intHistMap = new HashMap<Integer, Integer>();
+        Arrays.stream(vals).boxed().forEach(v -> intHistMap.put(v, intHistMap.getOrDefault(v, 0) + 1));
+        intHistMap.entrySet().stream()
+            .forEach(es -> sjHistMap.add("{Key: " + es.getKey() + " Value: " + es.getValue() + "}"));
+        System.out.println(sjHistMap.toString());
+
+    }
+}
