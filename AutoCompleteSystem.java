058f6f066c8ec023c8fc83d24a2fb7df960b3780
Tahsin Kabir <tahsinkabir@gmail.com>
2019-12-11 13:33:42 -0800

    Updated AutoCompleteSystem

--- AutoCompleteSystem.java
+++ AutoCompleteSystem.java
@@ -1,16 +1,33 @@
 import java.util.*;
+import java.util.stream.*;
 
 //https://leetcode.com/problems/design-search-autocomplete-system/submissions/
 
 class AutocompleteSystem {
 
+    int LIMIT = 3;
     TrieNode root = new TrieNode();
+    TrieNode current = root;
 
-    HashMap<String, Integer> sentenceUsage = new HashMap<String, Integer>();
+    HashMap<String, Integer> sentenceUsage = new HashMap<String, Integer>(); ;
+    StringBuilder searchString = new StringBuilder();;
 
     class TrieNode {
         HashMap<Character, TrieNode> nextNodes = new HashMap<Character, TrieNode>();
         HashSet<String> sentences = new HashSet<String>();
+    }
+
+    public List<String> input(char c) {
+        if (('a' <= c && c <= 'z') || (c == ' ')) {
+            searchString.append(c);
+            current = current.nextNodes.computeIfAbsent(c, tn -> new TrieNode());
+            return getTopNSentences(LIMIT, current.sentences);
+        } else if (c == '#') {
+            searchString = new StringBuilder();
+            current = this.root;
+            addSentence(this.root, searchString.toString(), 1);
+        }
+        return new ArrayList<>();
     }
 
     private void addSentence(TrieNode node, String sentence, int usage) {
@@ -22,61 +39,36 @@ class AutocompleteSystem {
         }
     }
 
-    int LIMIT = 3;
-    StringBuilder searchString = new StringBuilder();
-
     public AutocompleteSystem(String[] sentences, int[] times) {
+        IntStream.range(0, sentences.length)
+            .forEach( i -> addSentence(this.root, sentences[i], times[i]));
+        /*
         for (int i = 0; i < sentences.length; i++) {
-            addSentence(this.root, sentences[i], times[i]);
         }
-    }
-
-    TrieNode current = root;
-
-    public List<String> input(char c) {
-        if ( ('a' <= c && c <= 'z') || ( c == ' ') ) {
-            current = current.nextNodes.computeIfAbsent(c, tn -> new TrieNode());
-            searchString.append(c);
-            return getTopNSentences(LIMIT, current.sentences);
-        }
-
-        if (c == '#') {
-            current = this.root;
-            addSentence(this.root, searchString.toString(), 1);
-            searchString = new StringBuilder();
-        }
-        return new ArrayList<>();
+        */
     }
 
     class SentenceUsage {
         int usage;
-        String sent;
-        public SentenceUsage(String sent, int usage) { this.sent = sent; this.usage = usage;};
+        String sentence;
+        public SentenceUsage(String sentence, int usage) { this.sentence = sentence; this.usage = usage;};
     }
 
-    public List<String> getTopNSentences(int N, HashSet<String> sentences) {
+    private List<String> getTopNSentences(int N, HashSet<String> sentences) {
 
-        List<SentenceUsage> usage = new ArrayList();
-
-        for (String sentence: sentences) {
-            int usageCount = sentenceUsage.get(sentence);
-            usage.add(new SentenceUsage(sentence, usageCount));
-        }
+        List<SentenceUsage> usage = sentences.stream()
+            .map(s -> new SentenceUsage(s, sentenceUsage.get(s)))
+            .collect(Collectors.toList());
 
         Collections.sort(usage, (a, b) -> {
             int comp = Integer.compare(b.usage, a.usage);
-            return comp != 0? comp : a.sent.compareTo(b.sent);
+            return comp != 0? comp : a.sentence.compareTo(b.sentence);
         });
 
-        ArrayList<String> toReturn = new ArrayList<String>();
-        for (int i = 0; i < usage.size(); i++) {
-            if (i == N) {
-                break;
-            }
-            toReturn.add(usage.get(i).sent);
-        }
-
-        return toReturn;
+        return usage.stream()
+            .map(n -> n.sentence)
+            .limit(N)
+            .collect(Collectors.toList());
     }
 }
 
