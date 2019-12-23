6f7c7b998870bb00aab9c5480ccfbb1e340df787
Tahsin Kabir <tahsinkabir@gmail.com>
2019-12-22 19:28:29 -0800

    Updated find duplicate files

--- FindDuplicateFiles.java
+++ FindDuplicateFiles.java
@@ -4,7 +4,13 @@ import java.util.*;
 class FindDuplicateFiles {
 
     public static void main(String[] args) {
-        System.out.println("Compiling");
+        String[] paths = { 
+            "root/a 1.txt(abcd) 2.txt(efgh)",
+            "root/c 3.txt(abcd)",
+            "root/c/d 4.txt(efgh)",
+            "root 4.txt(efgh)"
+        };
+        System.out.println(new FindDuplicateFiles().findDuplicate(paths));
     }
 
     public List<List<String>> findDuplicate(String[] paths) {
@@ -12,13 +18,16 @@ class FindDuplicateFiles {
         HashMap<String, Set<String>> contentToFiles = new HashMap<String, Set<String>>();
 
         for (String path: paths) {
-            String[] pathElements = path.split(" ");
-            String directory = pathElements[0];
-            for (int i = 1; i < pathElements.length; i++) {
-                int start = pathElements[i].lastIndexOf("(");
-                int end = pathElements[i].lastIndexOf(")");
-                String filePath = directory + "/" + pathElements[i].substring(0, start);
-                String content = pathElements[i].substring(start + 1, end);
+            String[] pathElems = path.split(" ");
+            String dir = pathElems[0];
+            for (int i = 1; i < pathElems.length; i++) {
+                int start = pathElems[i].lastIndexOf("(");
+                String fileName = pathElems[i].substring(0, start);
+                String filePath = String.format("%s/%s", dir, fileName);
+
+                int end = pathElems[i].lastIndexOf(")");
+                String content = pathElems[i].substring(start + 1, end);
+
                 contentToFiles.computeIfAbsent(content, f -> new HashSet<String>())
                     .add(filePath);
             }
