07a2e54708dfc4a495db68d914dae8ee407c7a41
Tahsin Kabir <tahsinkabir@gmail.com>
2019-12-22 20:08:47 -0800

    Adding group anagram

--- GroupAnagram.java
+++ GroupAnagram.java
@@ -11,21 +11,20 @@ class GroupAnagram {
 
   public List<List<String>> groupAnagram(String[] words) {
 
-    HashMap<String, ArrayList<String>> charMapToString = new HashMap<String, ArrayList<String>>();
+    HashMap<String, ArrayList<String>> charMapToWord = new HashMap<String, ArrayList<String>>();
 
-    for(String word: words) {
+    for (String word: words) {
 
       short[] charFreq = new short[26];
       for(int i = 0; i < word.length(); i++) {
           charFreq[word.charAt(i) - 'a']++;
       }
 
-      String key = Arrays.toString(charFreq);
-      charMapToString.computeIfAbsent(key, v -> new ArrayList<>())
+      charMapToWord.computeIfAbsent(Arrays.toString(charFreq), v -> new ArrayList<>())
           .add(word);
     }
 
-    return new ArrayList<List<String>>(charMapToString.values());
+    return new ArrayList<List<String>>(charMapToWord.values());
   }
 }
 
