ed5ca579bbfb7ebe14a13b4ffc224523b9fff942
Tahsin Kabir <tahsinkabir@gmail.com>
2021-11-01 16:03:38 -0700

    refactz Nary common ancestor

--- CommonAncestorNary.java
+++ CommonAncestorNary.java
@@ -1,86 +1,75 @@
+/*
+    Find the common ancestor of two nodes in an N-ary tree, all nodes have unique values
+ */
 import java.util.*;
 
-public class CommonAncestorNary {
- 
-    public class Node {
-        String val;
-        Set<Node> next = new HashSet<>();
-        public Node(String val) { this.val = val;}
+class Node {
+    String val;
+    Set<Node> next = new HashSet<>();
+    public Node(String val) { this.val = val;}
+}
+
+class AncestorRet {
+    String val;
+    boolean foundBoth = false;
+    public AncestorRet(String val, boolean foundBoth) { this.val = val; this.foundBoth = foundBoth;}
+}
+
+class CommonAncestorNary {
+
+    public static void main(String[] args) {
+        Node root = new CommonAncestorNary().createTree();
+        System.out.println("Common Ancestor: " + new CommonAncestorNary().findAncestor(root, "B0D1E0", "B0D0E1").val);
+        System.out.println("Common Ancestor: " + new CommonAncestorNary().findAncestor(root, "B0D1E0", "B0D1E1").val);
+        System.out.println("Common Ancestor: " + new CommonAncestorNary().findAncestor(root, "B0D0", "C0").val);
     }
 
-    public class AncestorRet02 {
-        boolean found = false;
-        String ancestor;
-        public AncestorRet02(String ancestor) {this.ancestor = ancestor;} 
-        public AncestorRet02(String ancestor, boolean found) { this.found = found; this.ancestor = ancestor;} 
-    }
-
-    public AncestorRet02 findAncestor02(Node root, String loc1, String loc2) {
-
-        if (loc1 == null) { //loc1 should not be null
-            return null; 
+    public AncestorRet findAncestor(Node root, String val1, String val2) {
+        if (root.val.equals(val1) || root.val.equals(val2)) {
+            String match = root.val.equals(val1)? val1: val2;
+            String toFind = root.val.equals(val1)? val2: val1;
+            for (Node next: root.next) {
+                boolean found = inSubtree(next, toFind);
+                if (found) {
+                    return new AncestorRet(root.val, true); //current node is the common ancestor
+                }
+            }
+            return new AncestorRet(match, false);  //single value match with the current node
         }
 
-        if (loc2 == null) {
-            if (root.val.equals(loc1)) { //loc1 is matches with the current node
-                return new AncestorRet02(loc1); 
-            } 
-
-            for (Node next: root.next) {
-                AncestorRet02 ret = findAncestor02(next, loc1, null);
-                if (ret != null) { //loc1 matches with some node down the tree 
-                    return ret;
-                }
-            }
-            return null; //no matches under this root 
-        }
-
-        if (root.val.equals(loc1)) { 
-            for (Node next: root.next) {
-                AncestorRet02 res = findAncestor02(next, loc2, null);
-                if (res != null) {
-                    return new AncestorRet02(root.val, true); //current node is the common ancestor
-                }
-            }
-            return new AncestorRet02(loc1);  //loc1 is matches with the current node
-        } 
-
-        if (root.val.equals(loc2)) {
-            for (Node next: root.next) {
-                AncestorRet02 res = findAncestor02(next, loc1, null);
-                if (res != null) {
-                    return new AncestorRet02(root.val, true); //current node is the common ancestor
-                }
-            }
-            return new AncestorRet02(loc2);  //loc2 is matches with the current node
-        } 
-
-        List<AncestorRet02> results = new ArrayList<>();
+        /* no val1 or val2 match */
+        List<AncestorRet> results = new ArrayList<>();
         for (Node next: root.next) {
-            AncestorRet02 res = findAncestor02(next, loc1, loc2);
+            AncestorRet res = findAncestor(next, val1, val2);
             if (res != null) {
                 results.add(res);
             }
         }
 
-        if (results.size() == 0) { //no matches under this root 
-            return null;
-        } else if (results.size() == 1) { //one match under this root 
+        if (results.size() == 2) { //2 matches, current node is the common ancestor
+            return new AncestorRet(root.val, true);
+        } else if (results.size() == 1) { //maybe an ancestor or a match found, we pass it above
             return results.get(0);
         } else {
-            return new AncestorRet02(root.val, true); //current node is the common ancestor
+            return null; //no matches under this root
         }
     }
 
+    public boolean inSubtree(Node node, String val) {
+        if (node.val.equals(val)) { //val match on the node
+            return true;
+        }
 
-    /* Helper functions to aid in tests */
-    public static void main(String[] args) {
-        Node root = new CommonAncestorNary().createTree();
-        System.out.println("Common Ancestor: " + new CommonAncestorNary().findAncestor02(root, "B0D1E0", "B0D0E1").ancestor);
-        System.out.println("Common Ancestor: " + new CommonAncestorNary().findAncestor02(root, "B0D1E0", "B0D1E1").ancestor);
-        System.out.println("Common Ancestor: " + new CommonAncestorNary().findAncestor02(root, "B0D0", "C0").ancestor);
+        for (Node next: node.next) {
+            boolean found = inSubtree(next, val);
+            if (found) {
+                return true;
+            }
+        }
+        return false;
     }
 
+    /* Helper functions to aid in testing */
     private Node createTree() {
         Node root = new Node("A");
         Set<Node> bNodes = createNodes("B", 4);
@@ -107,4 +96,3 @@ public class CommonAncestorNary {
         return nodeSet;
     }
 }
-
