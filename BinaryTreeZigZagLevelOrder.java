ea7d93bbce5b5902f6d02da943b6cb3ddc2999b6
Tahsin Kabir <tahsinkabir@gmail.com>
2021-11-01 00:48:44 -0700

    Refactor to BinaryTreeZigZagLevelOrder

--- BinaryTreeZigZagLevelOrder.java
+++ BinaryTreeZigZagLevelOrder.java
@@ -1,60 +1,61 @@
 import java.util.*;
 
-//Check Leetcode: https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
+/*
+ https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
+ print nodes at the same depth/level alternating from left to right and
+ then right to left..
+*/
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+class NodeDepth {
+    TreeNode tnode;
+    int depth = 0;
+    NodeDepth(TreeNode tnode, int depth) {
+        this.tnode = tnode;
+        this.depth = depth;
+    }
+}
 
 class BinaryTreeZigZagLevelOrder {
 
-    class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-        TreeNode(int x) { val = x; }
-    }
-
-    class Node {
-        TreeNode tnode;
-        int depth = 0;
-        Node(TreeNode tnode) { this.tnode = tnode; }
-    }
-
-    /* TreeMap keeps the order of lists by depth */
-    TreeMap<Integer, LinkedList<Integer>> map = new TreeMap<>();
-
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-
         if (root == null) {
             return new ArrayList<>();
         }
 
-        Queue<Node> queue = new LinkedList<>();
-        queue.offer(new Node(root));
+        /* to keeps the order of lists by depth */
+        TreeMap<Integer, LinkedList<Integer>> nodesByDepth = new TreeMap<>();
+
+        Queue<NodeDepth> queue = new LinkedList<>();
+        queue.offer(new NodeDepth(root, 0));
 
         while (queue.size() > 0) {
-
-            Node node = queue.poll();
-
-            LinkedList<Integer> list = map.computeIfAbsent(node.depth, l -> new LinkedList<>());
+            NodeDepth node = queue.poll();
+            LinkedList<Integer> list = nodesByDepth.computeIfAbsent(node.depth, l -> new LinkedList<>());
 
             /* Starting from 0th node, left -> right */
             if (node.depth % 2 == 0) {
-                list.offerLast(node.tnode.val);
+                list.offerLast(node.tnode.val); //left to right
             } else {
-                list.offerFirst(node.tnode.val);
+                list.offerFirst(node.tnode.val); //right to left
             }
 
             if (node.tnode.left != null) {
-                Node leftNode = new Node(node.tnode.left);
-                leftNode.depth = node.depth + 1;
+                NodeDepth leftNode = new NodeDepth(node.tnode.left, node.depth + 1);
                 queue.offer(leftNode);
             }
 
             if (node.tnode.right != null) {
-                Node rightNode = new Node(node.tnode.right);
-                rightNode.depth = node.depth + 1;
+                NodeDepth rightNode = new NodeDepth(node.tnode.right, node.depth + 1);
                 queue.offer(rightNode);
             }
         }
-        return new LinkedList(map.values());
+        return new LinkedList(nodesByDepth.values());
     }
 }
 
