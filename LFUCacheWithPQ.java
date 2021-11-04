1872a679591a05970c6a9ea4d4a246c53b013f73
Tahsin Kabir <tahsinkabir@gmail.com>
2021-11-03 22:01:58 -0700

    Updated LFU cache

--- LFUCacheWithPQ.java
+++ LFUCacheWithPQ.java
@@ -1,49 +1,48 @@
 import java.util.*;
 
-class LFUCacheWithPQ {
+class KeyFrequency {
+    int key;
+    int frequency;
+    int value;
+    int time;
 
-    class KeyFrequency {
-        int key;
-        int frequency;
-        int value;
-        int time;
-
-        public KeyFrequency(int key, int value, int frequency, int time) {
-            this.key = key;
-            this.value = value;
-            this.frequency = frequency;
-            this.time = time;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            KeyFrequency kf = (KeyFrequency) o;
-            return Objects.equals(this.key, kf.key);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(this.key);
-        }
-
-        @Override
-        public String toString() {
-            return String.format("{Key:%s, Value:%s, Frequency:%s, Time:%s}", key, value, frequency, time);
-        }
+    public KeyFrequency(int key, int value, int frequency, int time) {
+        this.key = key;
+        this.value = value;
+        this.frequency = frequency;
+        this.time = time;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        KeyFrequency kf = (KeyFrequency) o;
+        return Objects.equals(this.key, kf.key);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.key);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("{Key:%s, Value:%s, Frequency:%s, Time:%s}", key, value, frequency, time);
+    }
+}
+
+class LFUCacheWithPQ {
     int capacity = 0, logicalTime = 0;
     HashMap<Integer, KeyFrequency> dataMap = new HashMap<Integer, KeyFrequency>(); 
     PriorityQueue<KeyFrequency> pq;
 
     public LFUCacheWithPQ(int capacity) {
         this.capacity = capacity;
-        this.pq = new PriorityQueue<KeyFrequency>((a, b) -> {
-            int comp = Integer.compare(a.frequency, b.frequency); /* least frequent first */
-            return comp != 0? comp : Integer.compare(a.time, b.time); /* least recent first */
-        });
+        this.pq = new PriorityQueue<KeyFrequency>(
+            Comparator.comparingInt((KeyFrequency a) -> a.frequency) /* least frequent first */
+                .thenComparingInt(a -> a.time) /* least recent first */
+        );
     }
 
     public int get(int key) {
@@ -61,13 +60,10 @@ class LFUCacheWithPQ {
     }
 
     public void put(int key, int value) {
-
         if (capacity == 0) {
             return;
         }
-
         logicalTime++;
-
         if (dataMap.containsKey(key)) {
             KeyFrequency kf = dataMap.get(key);
             pq.remove(kf);
