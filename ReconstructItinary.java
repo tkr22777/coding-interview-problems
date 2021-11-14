106e0960ababe995ff3093f1dc12e26144313785
Tahsin Kabir <tahsinkabir@gmail.com>
2021-11-13 20:50:20 -0800

    Added comment for reconstruct ininary

--- ReconstructItinary.java
+++ ReconstructItinary.java
@@ -3,20 +3,22 @@
  */
 import java.util.*;
 
-/* TODO add explanation */
 class ReconstructItinary {
     public static void main(String[] args) {
-        /*{ {"EZE","TIA"},{"EZE","HBA"},{"AXA","TIA"},{"JFK","AXA"}, {"ANU","JFK"},
+        /* {
+         *  {"EZE","TIA"},{"EZE","HBA"},{"AXA","TIA"},{"JFK","AXA"}, {"ANU","JFK"},
          *  {"ADL","ANU"},{"TIA","AUA"},{"ANU","AUA"},{"ADL","EZE"},{"ADL","EZE"},
          *  {"EZE","ADL"},{"AXA","EZE"},{"AUA","AXA"},{"JFK","AXA"},{"AXA","AUA"},
-         *  {"AUA","ADL"},{"ANU","EZE"},{"TIA","ADL"},{"EZE","ANU"},{"AUA","ANU"}}
+         *  {"AUA","ADL"},{"ANU","EZE"},{"TIA","ADL"},{"EZE","ANU"},{"AUA","ANU"}
+         * }
          */
     }
 
     public List<String> findItinerary(List<List<String>> tickets) {
         int totalTickets = tickets.size();
-        Map<String, List<String>> departToArrive = new HashMap<String, List<String>>();
+        Map<String, ArrayList<String>> departToArrive = new HashMap<String, ArrayList<String>>();
 
+        //building the adjacency map
         for (List<String> ticket: tickets) {
             String depart = ticket.get(0);
             String arrive = ticket.get(1);
@@ -26,41 +28,31 @@ class ReconstructItinary {
 
         departToArrive.keySet()
             .forEach(k -> Collections.sort(departToArrive.get(k)));
-        //departToArrive.keySet().forEach(k -> System.out.println("Key: " + k + " Val: " + departToArrive.get(k).toString()));
         return findItineraryRecursive(departToArrive, "JFK", totalTickets);
     }
 
-    public ArrayList<String> findItineraryRecursive(Map<String, List<String>> departToArrive, String depart, int totalTickets) {
+    private ArrayList<String> findItineraryRecursive(Map<String, ArrayList<String>> departToArrive, String depart, int totalTickets) {
+        //no tickets left, we are at the end of our itinerary!
         if (totalTickets == 0) {
             return new ArrayList<>(Arrays.asList(depart));
         }
 
-        if (!departToArrive.containsKey(depart)) { //unacceptable base case, no ticket with such departure; empty itinerary
-            return new ArrayList<>(); 
-        }
-
-        List<String> arrivals = departToArrive.get(depart);
-        List<String> arrivalCopy = new ArrayList<>(arrivals);
-
-        ArrayList<String> anItin = null;
-
-        for (String arrival: arrivalCopy) {
-            arrivals.remove(arrival);
-
-            anItin = findItineraryRecursive(departToArrive, arrival, totalTickets - 1);
-
-            arrivals.add(arrival);
-
-            if (anItin.size() > 0) {
-                break;
-            }
-        }
-
-        if (anItin.size() <= 0) {
+        //unacceptable base case, no ticket with the current departure; empty itinerary
+        if (!departToArrive.containsKey(depart)) {
             return null;
         }
 
-        anItin.add(0, depart);
-        return anItin;
+        ArrayList<String> arrivals = departToArrive.get(depart);
+        for (int i = 0; i < arrivals.size(); i++) {
+            String newDepart = arrivals.get(i);
+            arrivals.remove(i); //removing the newDepart prior to rest of calculation
+            ArrayList<String> anItin = findItineraryRecursive(departToArrive, newDepart, totalTickets - 1);
+            arrivals.add(i, newDepart);
+            if (anItin != null) { // found an valid itinerary
+                anItin.add(0, depart); //adding to front
+                return anItin;
+            }
+        }
+        return null;
     }
 }
