779246de57e8f584f10944610a4323c74bcfc671
Tahsin Kabir <tahsinkabir@gmail.com>
2025-02-13 22:15:43 -0500

    updates to group anagram

--- 00_INITIALLY_REVIEWED/GroupAnagram.py
+++ 00_INITIALLY_REVIEWED/GroupAnagram.py
@@ -3,8 +3,8 @@ class Solution(object):
     def groupAnagrams(self, strs):
         outMap = {}
         for str in strs:
-            key = ''.join(sorted(strs[i]))
-            outMap.setdefault(key, []).append(strs[i])
+            key = ''.join(sorted(str))
+            outMap.setdefault(key, []).append(str)
 
         output = []
         for val in outMap.values():
