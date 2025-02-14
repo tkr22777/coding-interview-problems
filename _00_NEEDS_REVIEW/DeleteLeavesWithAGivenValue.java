a4a064651f13ef9c3bce478ff1fa1ec0ff720931
Tahsin Kabir <tahsinkabir@gmail.com>
2025-02-14 13:06:59 -0500

    added _00_NEEDS_REVIEW codes

---
+++ _00_NEEDS_REVIEW/DeleteLeavesWithAGivenValue.java
@@ -0,0 +1,23 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class DeleteLeavesWithAGivenValue {
+    public static void main(String[] args) {
+        System.out.println("compiling");
+
+    }
+
+    public TreeNode removeLeafNodes(TreeNode root, int target) {
+
+    }
+}
