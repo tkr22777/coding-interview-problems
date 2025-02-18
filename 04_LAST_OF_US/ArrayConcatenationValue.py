2dd6185a3a5b3ea99edbdcb88635e280079b9fca
Tahsin Kabir <tahsinkabir@gmail.com>
2025-02-18 11:38:01 -0500

    some more updates

--- 04_LAST_OF_US/ArrayConcatenationValue.py
+++ 04_LAST_OF_US/ArrayConcatenationValue.py
@@ -1,5 +1,7 @@
 from typing import List
 
+# https://leetcode.com/problems/find-the-array-concatenation-value/
+
 class Solution:
     def findTheArrayConcVal(self, nums: List[int]) -> int:
         i = 0
@@ -9,11 +11,13 @@ class Solution:
             total += int(str(nums[i]) + str(nums[j]))
             i += 1
             j -= 1
+
         if i == j:
             total += nums[i]
+
         return total
 
 # Test cases
 s = Solution()
-print(s.findTheArrayConcVal([7,52,2,4]) == 596)
-print(s.findTheArrayConcVal([5,14,13,8,12]) == 673) 
+print(s.findTheArrayConcVal([7, 52, 2, 4]) == 596)
+print(s.findTheArrayConcVal([5, 14, 13, 8, 12]) == 673)
--- 04_LAST_OF_US/AverageOfSubtree.py
+++ 04_LAST_OF_US/AverageOfSubtree.py
@@ -1,4 +1,4 @@
-## https://leetcode.com/problems/count-nodes-equal-to-average-of-subtree/
+# https://leetcode.com/problems/count-nodes-equal-to-average-of-subtree/
 
 # Definition for a binary tree node.
 class TreeNode(object):
@@ -7,13 +7,11 @@ class TreeNode(object):
         self.left = left
         self.right = right
 
+# Given the root of a binary tree, return the number of nodes where the value
+# of the node is equal to the average of the values in its subtree.
 class Solution(object):
-    def averageOfSubtree(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
-        return self.averageOfSubtreeHelper(root)[2]
+    def averageOfSubtree(self, root) -> int:
+       return self.averageOfSubtreeHelper(root)[2]
         
     def averageOfSubtreeHelper(self, node):
         
@@ -25,6 +23,7 @@ class Solution(object):
 
         count = 1
         total = node.val
+        # result is the number of nodes that have the same value as the average of the subtree
         result = 0
 
         if l_count:
@@ -37,9 +36,33 @@ class Solution(object):
             total += r_total
             result += r_result
 
-        average = total / count
+        # average is considered the floor on this problem
+        average = int(total / count)
+
+        # print(f"node: {node.val}, count: {count}, total: {total}, result: {result}, average: {average}")
     
         if node.val == average:
+            print(f"found a match for {node.val} with average {average}")
             return (count, total, result + 1)
         else:
             return (count, total, result)
+
+# Test case
+if __name__ == "__main__":
+    # Create a small test tree:
+    #       4
+    #      / \
+    #     8   5
+    #    / \   \
+    #   0   1   6
+    
+    root = TreeNode(4)
+    root.left = TreeNode(8)
+    root.right = TreeNode(5)
+    root.right.right = TreeNode(6)
+    root.left.left = TreeNode(0)
+    root.left.right = TreeNode(1)
+    
+    solution = Solution()
+    result = solution.averageOfSubtree(root)
+    print(f"Number of nodes equal to average of subtree: {result}, valid: {result == 5}")
