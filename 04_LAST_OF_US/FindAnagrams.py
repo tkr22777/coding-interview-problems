56429d2dd009d5fbff5aa3b836053d5585855f84
Tahsin Kabir <tahsinkabir@gmail.com>
2025-02-20 10:40:16 -0500

    Adding find anagrams

--- 04_LAST_OF_US/FindAnagrams.py
+++ 04_LAST_OF_US/FindAnagrams.py
@@ -1,7 +1,12 @@
 from typing import List
 
+# https://leetcode.com/problems/find-all-anagrams-in-a-string/
+# Such a good mid-level engineering problem.
+# Good exmaple of a sliding window problem.
+# Good example of converting characters to unicode/indices.
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
+        # print(f"s len: {len(s)} p len: {len(p)}")
         if len(p) > len(s):
             return []
 
@@ -9,19 +14,28 @@ class Solution:
         p_rep = 26 * [0]
         for ch in p:
             p_rep[ord(ch) - ord('a')] += 1
-        # print(p_rep)
 
-        s_win_rep = 26 * [0]
+        s_window_rep = 26 * [0]
         for i in range(len(p) - 1):
-            s_win_rep[ord(s[i]) - ord('a')] += 1
+            s_window_rep[ord(s[i]) - ord('a')] += 1
 
+        # i goes from 0 to len(s) - len(p)
         for i in range(len(s) - len(p) + 1):
             j = i + len(p) - 1
             # print(f"i: {i} j: {j} diff: {diff}")
-            s_win_rep[ord(s[j]) - ord('a')] += 1
+
+            # Add the rightmost character to the window
+            s_window_rep[ord(s[j]) - ord('a')] += 1
+
             # print(s_win_rep)
-            if p_rep == s_win_rep:
+            if p_rep == s_window_rep:
                 indices.append(i)
-            s_win_rep[ord(s[i]) - ord('a')] -= 1
+            
+            # Remove the leftmost character from the window
+            s_window_rep[ord(s[i]) - ord('a')] -= 1
 
         return indices 
+
+s = Solution()
+print(s.findAnagrams("cbaebabacd", "abc") == [0, 6])
+print(s.findAnagrams("abab", "ab") == [0, 1, 2])
