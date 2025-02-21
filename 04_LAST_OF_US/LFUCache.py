b07d8c7a11abf4a21331f7857bdaa6ce89b3a325
Tahsin Kabir <tahsinkabir@gmail.com>
2025-02-21 14:47:23 -0500

    tmp

--- 04_LAST_OF_US/LFUCache.py
+++ 04_LAST_OF_US/LFUCache.py
@@ -5,23 +5,23 @@ from datetime import datetime
 
 
 # we need to store: key, value, frequency, last_used
-# store key -> value, frequency and last_usedata mapping in a dict
+# store key -> value, frequency and last_used data mapping in a dict
 # store frequency and timestamp on an heap
 
 class Entry:
     def __init__(self, key, value):
         self.key = key
         self.value = value
-        self.usage = 1
+        self.frequency = 1
         self.timestamp = datetime.now()
 
     def increment_usage(self):
-        self.usage += 1
+        self.frequency += 1
         self.timestamp = datetime.now()
     
     def __lt__(self, other):
-        if self.usage != other.usage:
-            return self.usage < other.usage
+        if self.frequency != other.frequency:
+            return self.frequency < other.frequency
         else:
             return self.timestamp < other.timestamp
 
@@ -35,13 +35,15 @@ class LFUCache:
  
     def set(self, key: str, value: str) -> str :
         if key in self.map:
-            val_entry = self.map[key] 
+            val_entry = self.map[key] # type: Entry
             val_entry.value = value
             val_entry.increment_usage()
+            # re-heapifying as frequency and timestamp have been updated
             heapq.heapify(self.heap)
         else:
             # re-heapifying in-case get calls updated frequency, timestamp
             heapq.heapify(self.heap)
+
             if len(self.map) == self.capacity:
                 del_entry = heapq.heappop(self.heap)
                 del self.map[del_entry.key]
@@ -63,90 +65,22 @@ class LFUCache:
         else: 
             return None
 
-class TestLFUCache(unittest.TestCase):
-    def test_set_and_get_2(self):
-        cache = LFUCache(2)
-        cache.set("a", "a1")
-        cache.set("a", "a2")
-        self.assertEqual(cache.get('a'), "a2")
-        cache.set("b", "b1")
-        self.assertEqual(cache.get('a'), "a2")
-        self.assertEqual(cache.get('b'), "b1")
-        # b should be removed,
-        # key 'a' has been accessed 4 times,
-        # key 'b' has been accessed 3 times
-        cache.set("c", "c1") 
-        self.assertEqual(cache.get('c'), "c1")
-        self.assertIsNone(cache.get('b'))
-        cache.get("c")
-        cache.get("c")
-        cache.set("d", "d1")
-        self.assertIsNone(cache.get("a"))
-        self.assertEqual(cache.get('d'), "d1")
-        self.assertEqual(cache.get('c'), "c1")
-        # self.assertIsNone(cache.get('b'))
-        # self.assertEqual(cache.get('a'), "1")
-        # cache.get("c")
-        # cache.set("d", "4")
-        # self.assertIsNone(cache.get('a'))
-    
-    
-    def test_set_and_get(self):
-        cache = LFUCache(3)
-        cache.set('a', 'apple')
-        cache.set('b', 'banana')
-        cache.set('c', 'cherry')
-        self.assertEqual(cache.get('a'), 'apple')
-        self.assertEqual(cache.get('b'), 'banana')
-        self.assertEqual(cache.get('c'), 'cherry')
-    
-    def test_eviction_least_frequently_used(self):
-        cache = LFUCache(2)
-        cache.set('a', 'apple')
-        cache.set('b', 'banana')
-        cache.get('a')  # Increase frequency of 'a'
-        cache.set('c', 'cherry')  # Should evict 'b'
-        self.assertIsNone(cache.get('b'), "Least frequently used (b) should be evicted.")
-        self.assertEqual(cache.get('a'), 'apple')
-        self.assertEqual(cache.get('c'), 'cherry')
-    
-    def test_update_value(self):
-        cache = LFUCache(2)
-        cache.set('a', 'apple')
-        cache.set('a', 'avocado')  # Update the value of 'a'
-        self.assertEqual(cache.get('a'), 'avocado', "Value of 'a' should be updated to 'avocado'.")
-
-    def test_eviction_with_same_frequency(self):
-        cache = LFUCache(2)
-        cache.set('a', 'apple')
-        cache.set('b', 'banana')
-        cache.get('a')  # 'a' and 'b' now have the same frequency
-        cache.get('b')
-        cache.set('c', 'cherry')  # Should evict the least recently used among 'a' and 'b'
-        self.assertTrue(cache.get('b') and not cache.get('a'), "Should evict 'a', the less recently used.")
-
-    def test_no_eviction_when_not_full(self):
-        cache = LFUCache(3)
-        cache.set('a', 'apple')
-        cache.get('a')  # Frequency of 'a' is now 2
-        cache.set('b', 'banana')  # No eviction necessary
-        self.assertEqual(cache.get('a'), 'apple')
-        self.assertEqual(cache.get('b'), 'banana')
-        
-    
-    def test_eviction_when_full_multiple_times(self):
-        cache = LFUCache(2)
-        cache.set('a', 'apple')
-        cache.set('b', 'banana')
-        cache.get('a')  # 'a' freq = 2
-        cache.get('b')  # 'b' freq = 2
-        cache.set('c', 'cherry')  # Should evict 'a' or 'b', least recently used among highest frequency
-        cache.get('c')
-        cache.get('c')
-        cache.set('d', 'date')  # Should evict the other of 'a' or 'b' // that's wrong
-        self.assertIsNone(cache.get('a') and cache.get('b'), "One of 'a' or 'b' should be evicted.")
-        self.assertIn(cache.get('c'), ['cherry'])
-        self.assertIn(cache.get('d'), ['date'])
-
-if __name__ == '__main__':
-    unittest.main()
+cache = LFUCache(2)
+cache.set("a", "a1")
+cache.set("a", "a2")
+print(cache.get('a') == "a2")
+cache.set("b", "b1")
+print(cache.get('a') == "a2")
+print(cache.get('b') == "b1")
+# b should be removed,
+# key 'a' has been accessed 4 times,
+# key 'b' has been accessed 3 times
+cache.set("c", "c1") 
+print(cache.get('c') == "c1")
+print(cache.get('b') is None)
+cache.get("c")
+cache.get("c")
+cache.set("d", "d1")
+print(cache.get("a") is None)
+print(cache.get('d') == "d1")
+print(cache.get('c') == "c1")
--- _034_REVIEWED/LargestPalindromic.py
+++ _034_REVIEWED/LargestPalindromic.py
@@ -1,3 +1,5 @@
+# https://leetcode.com/problems/largest-palindromic-number/description/
+
 from collections import defaultdict
 import heapq
 
@@ -13,16 +15,20 @@ class Solution:
         odd_max = None
         for key, val in char_freq.items():
             # print(f"key: {key}, val:{val}")
-            if val % 2 == 1 and (odd_max == None or key > odd_max):
+            if val % 2 == 1 and (odd_max is None or key > odd_max):
                 odd_max = key
 
+            # Since heapq in Python is a min-heap, we push the negative of the digit value.
+            # This causes the largest digit to have the smallest (most negative) value,
+            # ensuring it appears at the front of the heap (i.e., we effectively create a max-heap).
             if val > 1:
                 heapq.heappush(even_max_heap, (-1 * int(key), val))
 
         # print(even_max_heap)
         prefix = []
 
-        if even_max_heap[0][0] != 0:
+        # Only proceed if heap is not empty and largest digit is not zero
+        if even_max_heap and even_max_heap[0][0] != 0:
             while len(even_max_heap) > 0:
                 key, val = heapq.heappop(even_max_heap)
                 key = -1 * key
@@ -40,6 +46,6 @@ class Solution:
 
 
 s = Solution()
-print(s.largestPalindromic("444947137"))
-print(s.largestPalindromic("00009"))
-print(s.largestPalindromic("0000997")) 
+print(s.largestPalindromic("444947137") == "7449447")
+print(s.largestPalindromic("00009") == "9")
+print(s.largestPalindromic("0000997") == "9007009")
