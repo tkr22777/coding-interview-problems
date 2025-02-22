7706c4e66757ce76865fdb2c07426fd3f42a71ce
Tahsin Kabir <tahsinkabir@gmail.com>
2025-02-22 00:59:43 -0500

    adding maximum triplet value

--- 04_LAST_OF_US/MaximumTripletValue.py
+++ 04_LAST_OF_US/MaximumTripletValue.py
@@ -1,35 +1,30 @@
-# https://leetcode.com/problems/maximum-value-of-an-ordered-triplet-ii/submissions/1261454295/
+# https://leetcode.com/problems/maximum-value-of-an-ordered-triplet-ii/description/
+
 from typing import List
 
 class Solution:
     def maximumTripletValue(self, nums: List[int]) -> int:
+        # keeping min_diff since it can be negative which can produce 
+        # do we need to keep min_diff since, all nums are positive and thus
+        # negatives will never amount to a max_res
+
         max_i = nums[0]
-        min_i = nums[0]
-        min_diff = max_i - nums[1]
-        max_diff = min_i - nums[1]
-        max_res = max(max_diff * nums[2], min_diff * nums[2])
+        max_diff = max_i - nums[1]
+        max_res = max_diff * nums[2]
+
         for j in range(2, len(nums) - 1):
             max_i = max(max_i, nums[j - 1])
-            min_i = min(min_i, nums[j - 1])
-
             max_diff = max(max_diff, max_i - nums[j])
-            min_diff = min(min_diff, min_i - nums[j])
-
-            max_res = max(max_res,
-                          max(
-                              max(max_diff, max_diff * nums[j + 1]),
-                              max(min_diff, min_diff * nums[j + 1])
-                          )
-                          )
-
-            # print(f"i: {j - 1}, max_i: {max_i}, min_i: {min_i}, max_diff: {max_diff}, j: {j}, min_diff: {min_diff}, k: {j+1}, max_res:{max_res} ")
+            max_res = max(max_res, max_diff * nums[j + 1])
+            # print(f"i: {j - 1}, nums[i]: {nums[j - 1]}, max_i: {max_i}, max_diff: {max_diff}, j: {j}, nums[j]: {nums[j]}, max_res:{max_res} ")
 
         if max_res < 0:
             return 0
+
         return int(max_res)
 
 
 s = Solution()
-print(s.maximumTripletValue([12, 6, 1, 2, 7]))
-print(s.maximumTripletValue([1, 10, 3, 4, 19]))
-print(s.maximumTripletValue([1, 2, 3])) 
+print(s.maximumTripletValue([12, 6, 1, 2, 7]) == 77)
+print(s.maximumTripletValue([1, 10, 3, 4, 19]) == 133)
+print(s.maximumTripletValue([1, 2, 3]) == 0) 
